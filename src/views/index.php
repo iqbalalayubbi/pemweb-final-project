78933a6f100274248dc912269ab8ab111cb9d820
iqbalalayubbi <iqbal.is415@gmail.com>
2024-05-17 22:42:29 +0700

    INITIALIZE-CREATE: setup tailwind and create landing page ui

---
+++ src/views/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../output.css" rel="stylesheet">
+    <title>Easy Task</title>
+</head>
+
+<body class="px-8">
+    <header class="flex justify-between items-center pt-5">
+        <h1 class="text-2xl font-semibold text-purple-600">Easy Task</h1>
+        <div class="flex gap-10 items-center">
+            <nav class="flex gap-8">
+                <a class="link" href="#">
+                    Feature
+                </a>
+                <a class="link" href="#">
+                    Service
+                </a>
+                <a class="link" href="#">
+                    How To Use
+                </a>
+            </nav>
+            <div class="flex gap-3">
+                <button type="button" class="btn-secondary">
+                    Signup
+                </button>
+                <button type="button" class="btn-primary">
+                    Login
+                </button>
+            </div>
+        </div>
+    </header>
+    <div class="mt-24 flex flex-col items-center gap-3 relative">
+        <h1 class="text-5xl font-extrabold text-center leading-snug">Make Your Task Easier <br>With <span class="text-purple-500 px-2">Easy Task</span> </h1>
+        <p class="text-center text-lg">manage your task <br> that can be easy in one application</p>
+        <button class="btn-primary mt-5">Manage Task Now</button>
+        <img src="../assets/ilustration.svg" alt="" class="absolute left-0 bottom-0">
+        <img src="../assets/ilustration-1.svg" alt="" class="absolute right-0 top-0">
+    </div>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7469B6",
+        "dark-normal": "#464646",
+        "white-gray": "#EEEEEE",
+      },
+    },
+  },
+  plugins: [],
+};
