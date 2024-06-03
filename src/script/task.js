c5fecf52db4c1144eae1173913f6a5a79f819bf1
iqbalalayubbi <iqbal.is415@gmail.com>
2024-06-03 22:21:15 +0700

    FEATURES: change title and delete project

--- src/script/task.js
+++ src/script/task.js
@@ -30,6 +30,11 @@ async function renderBlock() {
   const blocksData = await editor.configuration.renderBlock(blockId, username);
   if (blocksData) {
     editor.render(JSON.parse(blocksData.blocks_data));
+    $("#editorjs").css("display", "block");
+    $(".default-view").css("display", "none");
+  } else {
+    $("#editorjs").css("display", "none");
+    $(".default-view").css("display", "flex");
   }
 }
 
@@ -48,12 +53,18 @@ function createBlock() {
 }
 
 async function setActive(e) {
+  // button-project
   if ($(e.target).hasClass("btn-project")) {
     for (let i = 0; i < $(".btn-project").length; i++) {
       $(`.btn-project:eq(${i})`).removeClass("project-active");
     }
     $(e.target).addClass("project-active");
     await renderBlock();
+  }
+
+  // setting menu button
+  if ($(e.target).hasClass("setting-project")) {
+    showProjectMenu(e);
   }
 }
 
@@ -67,18 +78,17 @@ async function saveBlock(blockTitle) {
     username: $(".username-container").attr("data-username"),
     createdAt: moment().format("YYYY-MM-DD HH-mm-ss"),
   };
-  console.log(data.createdAt);
   await editor.configuration.saveBlock(data);
   await editor.clear();
   await updateUI();
   for (let i = 0; i < $(".btn-project").length; i++) {
-    console.log();
     if ($(`.btn-project:eq(${i})`).attr("data-blockId") == data.blockId) {
       $(`.btn-project:eq(${i})`).addClass("project-active");
     } else {
       $(`.btn-project:eq(${i})`).removeClass("project-active");
     }
   }
+  checkBlockActive();
 }
 
 async function updateUI() {
@@ -86,15 +96,27 @@ async function updateUI() {
   const result = await editor.configuration.getAllBlock(username);
   const allBlock = JSON.parse(result);
   let htmlElements = "";
+  if (allBlock.length <= 0) {
+    $("#editorjs").css("display", "none");
+    $(".default-view").css("display", "flex");
+    return $(".projects").html(htmlElements);
+  }
+
+  $("#editorjs").css("display", "block");
+  $(".default-view").css("display", "none");
   allBlock.forEach((block) => {
     htmlElements += `
       <button class="btn btn-project" role="button" data-blockId=${block.block_id}>
-          <img src="../assets/tutor-1.svg" alt="" width="30">
-          ${block.block_title}
-      </button>
+          <div class="contents">
+              <img src="../assets/tutor-1.svg" alt="" width="20">
+              <span class="title">${block.block_title}</span>
+          </div>
+          <iconify-icon icon="solar:menu-dots-bold" width="20" height="20" class="setting-project" data-title=${block.block_title} data-blockId=${block.block_id}></iconify-icon>
+      </button>    
     `;
     $(".projects").html(htmlElements);
   });
+  checkBlockActive();
 }
 
 async function updateBlock() {
@@ -106,6 +128,35 @@ async function updateBlock() {
     createdAt: moment().format("YYYY-MM-DD HH-mm-ss"),
   };
   await editor.configuration.updateBlock(data);
+}
+
+async function updateTitle(blockId, blockTitle) {
+  const data = {
+    blockId,
+    blockTitle,
+    username: $(".username-container").attr("data-username"),
+  };
+  await editor.configuration.updateTitle(data);
+}
+
+async function deleteBlock(blockId) {
+  const data = {
+    blockId,
+    username: $(".username-container").attr("data-username"),
+  };
+  await editor.configuration.deleteBlock(data);
+}
+
+function checkBlockActive() {
+  const children = $(".projects").has(".project-active");
+  if (children.length > 0) {
+    editor.render(JSON.parse(blocksData.blocks_data));
+    $("#editorjs").css("display", "block");
+    $(".default-view").css("display", "none");
+  } else {
+    $("#editorjs").css("display", "none");
+    $(".default-view").css("display", "flex");
+  }
 }
 
 function confirmLogout(event) {
@@ -120,6 +171,30 @@ function confirmLogout(event) {
       window.location.href = "logout.php";
     } else if (result.isDenied) {
       window.location.href = "logout.php";
+    }
+  });
+}
+
+function showProjectMenu(event) {
+  const blockId = event.target.getAttribute("data-blockId");
+  const blockTitle = event.target.getAttribute("data-title");
+
+  Swal.fire({
+    input: "text",
+    inputValue: blockTitle,
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Change Title",
+    denyButtonText: `Remove this project`,
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      // change title
+      await updateTitle(blockId, result.value);
+      await updateUI();
+    } else if (result.isDenied) {
+      // remove project
+      await deleteBlock(blockId);
+      await updateUI();
     }
   });
 }
