d3646831b517334da8ca9c2d764ee528d17ebab7
Muhammad Reza Fahlefi <reza3fahlevi@gmail.com>
2024-06-15 19:45:54 +0700

    nambah profile
    profile.php di views
    profile.css di css
    foto-profile.svg di assets

---
+++ src/views/profile.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/profile.css">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p class="username"><?php echo $_SESSION["username"] ?> </p>
+        <p class="email"> rezafahlevi@gmail.com </p>
+        <p class="gatau"> gatau ini apa </p>
+    </div>
+    <button class="save">save</button>
+    <button class="round-button"></button>
+</body>
+</html>
